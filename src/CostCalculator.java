093ea8432ec79e2b5c97f2999962af65dd9e048a
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-11 08:41:17 -0400

    Changed Class

--- src/CostCalculator.java
+++ src/CostCalculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class CostCalculator {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
