c54bf1f67c57dc245f4f995b6f267c81ae923fa5
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-11 08:40:17 -0400

    Hi

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double TAX = 0.05;
+        double itemPrice;
+        double totalPrice;
+
+        System.out.println("What is the price of your item");
+        itemPrice = scan.nextDouble();
+
+        totalPrice = itemPrice + itemPrice*TAX;
+        System.out.println("The total price of your item with the tax included is $" + totalPrice + ".");
+
+    }
+}
